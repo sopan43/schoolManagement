cef3c10c58bbc68869f51ba443f638258c7b9f33
Sopan Mittal <sopan.mittal@kroger.com>
2023-03-23 15:41:57 +0530

    check1

--- helper/options.js
+++ helper/options.js
@@ -2,6 +2,12 @@ module.exports = {
     formate: 'A4',
     orientation: 'portrait',
     border: '12mm',
+    timeout: '100000',
+    childProcessOptions: {
+        env: {
+          OPENSSL_CONF: '/dev/null',
+        },
+      }
     // header: {
     //     height: '15mm',
     //     contents: '<h4 style=" color: red;font-size:20;font-weight:800;text-align:center;">S.D.M. Public High School</h4>'
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ require('./config/passport')(passport);
 // Connecting to MongoDB...
 const mongoose = require('mongoose');
 
-const url = `mongodb://sopan:sopan@school-shard-00-00.jyw4b.mongodb.net:27017,school-shard-00-01.jyw4b.mongodb.net:27017,school-shard-00-02.jyw4b.mongodb.net:27017/sdmschool?ssl=true&replicaSet=atlas-w3e74l-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const url = `mongodb://sopanmittal43atlas:sopan@ac-7mqzik7-shard-00-00.wnzl4kq.mongodb.net:27017,ac-7mqzik7-shard-00-01.wnzl4kq.mongodb.net:27017,ac-7mqzik7-shard-00-02.wnzl4kq.mongodb.net:27017/sdmschool?ssl=true&replicaSet=atlas-vpq55e-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 const connectionParams={
     useNewUrlParser: true,
